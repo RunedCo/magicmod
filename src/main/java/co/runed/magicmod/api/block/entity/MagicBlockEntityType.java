896de3865c9ed8e6a238d0b2f84b9a51bdd730ee
Jono Kemball <jonathankemball@gmail.com>
2019-02-07 19:42:14 +1300

    add hell portal entity type

---
+++ src/main/java/co/runed/magicmod/api/block/entity/MagicBlockEntityType.java
@@ -0,0 +1,8 @@
+package co.runed.magicmod.api.block.entity;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+
+public class MagicBlockEntityType {
+    public static BlockEntityType<BlockEntity> DEMON_PORTAL;
+}
